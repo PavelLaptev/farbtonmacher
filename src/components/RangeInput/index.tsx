2460da6a3a1bf8561acaf92f133e07461914aa31
PavelLaptev <pawellaptew@gmail.com>
2023-05-12 02:44:55 +0200

    :construction: range input

--- src/components/RangeInput/index.tsx
+++ src/components/RangeInput/index.tsx
@@ -1,22 +1,63 @@
-import { useState, useEffect } from 'react'
-import styles from './styles.module.scss'
-
+import { useState, useEffect } from "react";
+import styles from "./styles.module.scss";
 
 interface Props {
-  className?: string
-  style?: React.CSSProperties
-  id: string
+  className?: string;
+  style?: React.CSSProperties;
+  label: string;
+  id: string;
+  max: number;
+  min: number;
+  value: number;
+  step: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-
-
 
 const RangeInput: React.FC<Props> = (props) => {
+  const [value, setValue] = useState(props.value);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(Number(e.target.value));
+    props.onChange(e);
+  };
 
   return (
-    <label htmlFor={props.id} className={`${styles.wrapper}`}>
-      <input id={props.id} type="range" min="1" max="10" className={styles.rangeInput} />
-    </label>
-  )
-}
+    <section className={`${styles.wrapper} ${props.className}`}>
+      <div className={styles.manualInputWrapper}>
+        <span className={styles.label}>{props.label}</span>
+        <input
+          type="number"
+          className={styles.manualInput}
+          value={value}
+          onChange={(e) => {
+            setValue(Number(e.target.value));
+            props.onChange(e);
+          }}
+        />
+      </div>
+      <label htmlFor={props.id} className={styles.rangeWrapper}>
+        <div className={styles.track}>
+          <div
+            className={styles.progress}
+            style={{
+              width: `${((value - props.min) / (props.max - props.min)) * 100}%`
+            }}
+          />
+        </div>
 
-export default RangeInput
+        <input
+          id={props.id}
+          type="range"
+          min={props.min}
+          max={props.max}
+          step={props.step}
+          className={styles.rangeInput}
+          value={value}
+          onChange={handleChange}
+        />
+      </label>
+    </section>
+  );
+};
+
+export default RangeInput;
--- src/components/ShadesBlock/index.tsx
+++ src/components/ShadesBlock/index.tsx
@@ -1,64 +1,71 @@
-import { useState, useEffect } from 'react'
-import ColorBlock from '../ColorBlock'
-import styles from './styles.module.scss'
+import { useState, useEffect } from "react";
+import ColorBlock from "../ColorBlock";
+import styles from "./styles.module.scss";
 
-import { RangeInput } from '@/components'
-import { generateColorShades, interpolateTo100 } from '@/utils'
-
+import { RangeInput } from "@/components";
+import { generateColorShades, interpolateTo100 } from "@/utils";
 
 interface Props {
-  steps: number
-  mainColor: string
-  direction: "lighten" | "darken"
+  steps: number;
+  mainColor: string;
+  direction: "lighten" | "darken";
 }
 
-
-
 const ShadesBlock: React.FC<Props> = (props) => {
-  const [shades, setShades] = useState([] as string[])
+  const [shades, setShades] = useState([] as string[]);
 
   const generateShadeIndexName = (index: number) => {
-    const totalShades = props.steps * 2 + 1
+    const totalShades = props.steps * 2 + 1;
 
-
-    if (props.direction === 'lighten') {
-      const shadeIndex = index + props.steps + 1
-      const percentage = interpolateTo100(totalShades, shadeIndex)
-      return `${percentage}`
+    if (props.direction === "lighten") {
+      const shadeIndex = index + props.steps + 1;
+      const percentage = interpolateTo100(totalShades, shadeIndex);
+      return `${percentage}`;
+    } else {
+      const shadeIndex = index + 1;
+      const percentage = interpolateTo100(totalShades, shadeIndex);
+      return `${percentage}`;
     }
-
-    else {
-      const shadeIndex = index + 1
-      const percentage = interpolateTo100(totalShades, shadeIndex)
-      return `${percentage}`
-    }
-  }
+  };
 
   useEffect(() => {
-    setShades(generateColorShades({
-      color: props.mainColor,
-      steps: props.steps,
-      direction: props.direction
-    }) as string[])
-
-  }, [props])
-
+    setShades(
+      generateColorShades({
+        color: props.mainColor,
+        steps: props.steps,
+        direction: props.direction
+      }) as string[]
+    );
+  }, [props]);
 
   return (
     <section className={styles.shadesWrapper}>
       <section className={styles.shades}>
-        {
-          shades.map((shade, index) => (
-            <ColorBlock key={index} color={shade} name={generateShadeIndexName(index)} />
-          ))
-        }
+        {shades.map((shade, index) => (
+          <ColorBlock
+            key={index}
+            color={shade}
+            name={generateShadeIndexName(index)}
+          />
+        ))}
       </section>
 
       <section className={styles.controls}>
-        <RangeInput id='shades-range-input' />
+        <RangeInput
+          id={`curve-${props.direction}`}
+          label="Curve"
+          className={styles.rangeInput}
+          min={0}
+          max={34}
+          step={1}
+          value={0}
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
       </section>
     </section>
-  )
-}
+  );
+};
 
-export default ShadesBlock
+export default ShadesBlock;
